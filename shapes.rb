9fc20614392eca8f42e09b0e4abaabe2414492fb
alfonso <alfonso.sr.trabajo@gmail.com>
2024-02-13 18:43:42 +0100

    First attempt with shapes

--- shapes.rb
+++ shapes.rb
@@ -1,20 +1,19 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Geometry < Formula
+class Shapes < Formula
   desc ""
   homepage ""
   url "https://github.com/alfonsoSR/geometry/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "47aff3e329e82ebf7eb8a258b5bd0670a1c5ec8f5c78f426516ecf46aebe7740"
   license ""
 
+  depends_on "cmake" => :build
+
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", "--disable-silent-rules", *std_configure_args
-	system "cmake", "-S", ".", "-B", "build"
-	system "make", "-C", "build"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 end
